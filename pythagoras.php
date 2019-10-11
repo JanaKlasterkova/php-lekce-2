e544cc7cc5bb3b8fbcda35bf1d91757a0b7188bf
jana_klasterkova <janaklasterkova@seznam.cz>
2019-10-11 14:17:49 +0200

    Obsah trojúhelníka

--- pythagoras.php
+++ pythagoras.php
@@ -2,5 +2,14 @@
 $a=5;
 $b=4;
 $obdelnikobsah = $a * $b;
-echo ('Obsah obdelníku o stranách '. $a. 'cm, ' . $b . 'cm je ' . $obdelnikobsah . 'cm2');
+echo ('Obsah obdelníku o stranách '. $a. 'cm, ' . $b . 'cm je ' . $obdelnikobsah . 'cm²');
+?>
+<br>
+    <br>
+<?php
+$strana=6;
+$uhel=60;
+$vyska=$strana*sin(deg2rad($uhel));
+$trojuhelnikobsah=$strana* ($vyska/2);
+echo ('Rovnostranný trojúhelník o straně délky ' . $strana . 'cm a úhlu ' . $uhel.' stupňů je '. round ($trojuhelnikobsah).'cm²');
     ?>
